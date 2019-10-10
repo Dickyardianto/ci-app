dd0dc7296ae65b8752efb3059cf9dd0a6959aa10
dicky Ardianto <dikiardianto19@gmail.com>
2019-10-10 17:15:23 +0700

    Merubah config autoload.php, config->database.php, config->routes.php, welcome.php, serta menambahkan file baru controller Home.php,Mahasiswa.php, Mahasiswa_model.php, home.index.php, mahasiswa->index.php, templates/footer.php, header.php

---
+++ application/views/home/index.php
@@ -0,0 +1,1 @@
+<h1>Hello, world! <?php echo $nama; ?></h1>
---
+++ application/views/mahasiswa/index.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Daftar Mahasiswa</h3>
+            <ul class="list-group">
+                <?php foreach ($mahasiswa as $mhs) : ?>
+                <li class="list-group-item"><?php echo $mhs['nama'] ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+</div>
